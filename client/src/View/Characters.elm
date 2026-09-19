36acd401b6cef851120de7ad36b3fbfbb1efe4d7
Kirk <tkshillinz@icloud.com>
2026-09-19 00:18:15 -0600

    adjust spacing of character sheet elements

--- client/src/View/Characters.elm
+++ client/src/View/Characters.elm
@@ -90,7 +90,7 @@ characterSheet facilitator myId aspectExamplesOpen ch =
         editable =
             facilitator || mine || ch.ownerId == Nothing
     in
-    Element.column [ spacing Ui.sm, width fill ]
+    Element.column [ spacing Ui.md, width fill ]
         [ ownerRow facilitator mine ch
         , boonsBlock facilitator ch
         , field editable ch NameField "" "Name" ch.name
